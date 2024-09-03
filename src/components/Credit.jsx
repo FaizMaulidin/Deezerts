3bbaed34aa44a46b0a71040bca635b83d39ae643
FaizMaulidin <famuld14@gmail.com>
2024-09-03 10:13:10 +0700

    fix credit link

--- src/components/Credit.jsx
+++ src/components/Credit.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Credit = () => {
   return (
-    <h1 className=' rounded-t-2xl bg-Red text-white font-default text-center py-12 px-4'>The design is provided by <a href='#' className='underline font-semibold'>Frontend Mentor</a></h1>
+    <h1 className=' rounded-t-2xl bg-Red text-white font-default text-center py-12 px-4'>The design is provided by <a href='https://www.frontendmentor.io/' target='blank' className='underline font-semibold'>Frontend Mentor</a></h1>
   )
 }
 
